3e29137aee74d97739bcc7de04fe3a39e794ffb5
robot <tov@calpoly.edu>
2025-10-02 19:54:50 -0700

    stuff

---
+++ Onboarding/Core/Inc/probe.h
@@ -0,0 +1,14 @@
+/*
+ * probe.h
+ *
+ *  Created on: Sep 28, 2025
+ *      Author: robot
+ */
+
+#ifndef INC_PROBE_H_
+#define INC_PROBE_H_
+
+void probe_write_byte();
+void probe_read_byte();
+
+#endif /* INC_PROBE_H_ */
