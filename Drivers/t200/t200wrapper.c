965cc4918564b4d3d420cf82297681b3842eb213
raymondLtran <raymondtral@gmail.com>
2025-10-26 16:10:43 -0700

    wow

---
+++ Drivers/t200/t200wrapper.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include "t200/t200driver.h"
+
+TIM_TypeDef thruster_timer;
+uint8_t thruster_timer
+void t200_setup(GPIO_TypeDef *GPIOx, uint32_t pin, TIM_TypeDef *TIMx, uint8_t channel) {
+	thruster_timer =
+	  PWM_Init(TIMx, GPIOx, pin, channel, 15, 19999, 0, 20, 1);
+}
---
+++ Drivers/t200/t200wrapper.h
@@ -0,0 +1,6 @@
+#ifndef INC_T200WRAPPER_H_
+#define INC_T200WRAPPER_H_
+
+void t200_wrapper(uint8_t speed, GPIO_TypeDef *GPIOx, uint32_t pin, uint8_t channel);
+
+#endif /* INC_T200WRAPPER_H_ */
