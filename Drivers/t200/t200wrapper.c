205ae7a08d01da613ce7abe23a49e27f39564863
raymondLtran <raymondtral@gmail.com>
2025-11-01 16:39:18 -0700

    Updated driver wrapper. Set Speed works and function to init all thruster pins.

--- Drivers/t200/t200wrapper.c
+++ Drivers/t200/t200wrapper.c
@@ -2,16 +2,15 @@
 #include "t200driver.h"
 
 void Thruster_Init() {
-	  PWM_Init(TIM2, GPIOA, 5, 1, 15, 19999, 0, 20, 1); // test pin to see if everything actually works
+	// Vertical Thruster
+	PWM_Init(TIM2, GPIOA, 15, 1, 15, 19999, 1, 28, 1);
+	PWM_Init(TIM2, GPIOA, 1, 2, 15, 19999, 0, 4, 1);
+	PWM_Init(TIM2, GPIOA, 2, 3, 15, 19999, 0, 8, 1);
+	PWM_Init(TIM2, GPIOA, 3, 4, 15, 19999, 0, 12, 1);
 
-	  PWM_Init(TIM2, GPIOA, 15, 1, 15, 1999, 1, 28, 1);
-	  PWM_Init(TIM2, GPIOA, 1, 2, 15, 1999, 0, 4, 1);
-	  PWM_Init(TIM2, GPIOA, 2, 3, 15, 1999, 0, 8, 1);
-	  PWM_Init(TIM2, GPIOA, 3, 4, 15, 1999, 0, 12, 1);
-
-	  // Horizontal Thruster
-	  PWM_Init(TIM3, GPIOA, 6, 1, 15, 3999, 0, 24, 2);
-	  PWM_Init(TIM3, GPIOA, 7, 2, 15, 3999, 0, 28, 2);
+	// Horizontal Thruster
+	PWM_Init(TIM3, GPIOA, 6, 1, 15, 19999, 0, 24, 2);
+	PWM_Init(TIM3, GPIOA, 7, 2, 15, 19999, 0, 28, 2);
 }
 
 void t200_setspeed(TIM_TypeDef *TIMx, uint8_t channel, uint32_t speed) {
--- Drivers/t200/t200wrapper.h
+++ Drivers/t200/t200wrapper.h
@@ -2,6 +2,6 @@
 #define INC_T200WRAPPER_H_
 
 void Thruster_Init();
-
+void t200_setspeed(TIM_TypeDef *TIMx, uint8_t channel, uint32_t speed);
 
 #endif /* INC_T200WRAPPER_H_ */
