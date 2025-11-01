3d272864542fdac22b30696bab9c654d1c5ae068
raymondLtran <raymondtral@gmail.com>
2025-11-01 16:05:48 -0700

    dd]wow

--- Drivers/t200/t200wrapper.c
+++ Drivers/t200/t200wrapper.c
@@ -1,11 +1,17 @@
 #include "main.h"
-#include "t200/t200driver.h"
+#include "t200driver.h"
 
-TIM_TypeDef thruster_timer;
-uint8_t thruster_timer
-void t200_setup(GPIO_TypeDef *GPIOx, uint32_t pin, TIM_TypeDef *TIMx, uint8_t channel) {
-	thruster_timer =
-	  PWM_Init(TIMx, GPIOx, pin, channel, 15, 19999, 0, 20, 1);
+void Thruster_Init() {
+	  PWM_Init(TIM2, GPIOA, 5, 1, 15, 19999, 0, 20, 1); // test pin to see if everything actually works
+
+	  PWM_Init(TIM2, GPIOA, 15, 1, 15, 1999, 1, 28, 1);
+	  PWM_Init(TIM2, GPIOA, 1, 2, 15, 1999, 0, 4, 1);
+	  PWM_Init(TIM2, GPIOA, 2, 3, 15, 1999, 0, 8, 1);
+	  PWM_Init(TIM2, GPIOA, 3, 4, 15, 1999, 0, 12, 1);
+
+	  // Horizontal Thruster
+	  PWM_Init(TIM3, GPIOA, 6, 1, 15, 3999, 0, 24, 2);
+	  PWM_Init(TIM3, GPIOA, 7, 2, 15, 3999, 0, 28, 2);
 }
 
 void t200_setspeed(uint32_t speed) {
