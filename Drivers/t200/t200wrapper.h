7b606706ee4e46e6deb668ced1824c8d5fb21334
Jason Tov <tov@calpoly.edu>
2026-01-24 15:28:00 -0800

    cleanup\

--- Drivers/t200/t200wrapper.h
+++
@@ -1,9 +0,0 @@
-#ifndef INC_T200WRAPPER_H_
-#define INC_T200WRAPPER_H_
-
-// TIM2 | Channel 1,2,3,4: Output Compare No Output | Prescaler: 15 if the chip clock is 16mhz
-// TIM3 | Channel 1,2: Output Compare No Output | Prescaler: 15 if the chip clock is 16mhz
-void Thruster_Init();
-void t200_setspeed(TIM_TypeDef *TIMx, uint8_t channel, int16_t speed);
-
-#endif /* INC_T200WRAPPER_H_ */
